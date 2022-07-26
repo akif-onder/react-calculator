63a31e776da25e315d5eddd92f7e2c3fb2f3550f
akif-onder <akifonder@gmail.com>
2022-07-26 17:40:52 +0300

    buttons added and styled

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import './App.css';
 import React from "react";
+import Button from './components/Button';
+import Input from './components/input';
 
 function App() {
   return (
     <div className="App">
       <div className='calc-wrapper'>
-        <h1>Calculator</h1>
+        <Input/>
+        <div className='row'>
+        <Button symbol='7'/>
+        <Button symbol='8'/>
+        <Button symbol='9'/>
+        <Button symbol='/'/>
+        </div>
+       
+        <div className='row'>
+        <Button symbol='4'/>
+        <Button symbol='5'/>
+        <Button symbol='6'/>
+        <Button symbol='*'/>
+        </div>
+
+        <div className='row'>
+        <Button symbol='1'/>
+        <Button symbol='2'/>
+        <Button symbol='3'/>
+        <Button symbol='+'/>
+        </div>
+        <div className='row'>
+        <Button symbol='0'/>
+        <Button symbol='.'/>
+        <Button symbol='='/>
+        <Button symbol='-'/>
+        </div>
+        
+
   
       </div>
 
---
+++ src/components/Button.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./Button.css";
+
+const Button = () => {
+  return (
+    <div className='button-wrapper'>{symbol}</div>
+  )
+}
+
+export default Button
