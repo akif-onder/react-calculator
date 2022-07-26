14697d8b562d1b5176cdc0189e8d1f8d19459a40
akif-onder <akifonder@gmail.com>
2022-07-26 17:22:15 +0300

    main component and its style added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import React from "react";
 
 function App() {
   return (
     <div className="App">
-      
+      <div className='calc-wrapper'>
+        <h1>Calculator</h1>
+  
+      </div>
+
     </div>
   );
 }
